61a91252afdf8c797b9d283fd3b19039d92af93a
legomyleg <pearsonroymorris@gmail.com>
2026-04-28 15:38:53 -0600

    Added color palettes

---
+++ colors.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include "raylib.h"
+
+struct ColorPalette {
+    const Color sceneBackgroundColor;
+    const Color panelBackgroundColor;
+    const Color panelColor;
+    const Color panelBorderColor;
+    const Color screenDividerColor;
+
+    constexpr ColorPalette(Color sbc, Color pbc, Color pc, Color pbdc, Color sdc) : 
+    sceneBackgroundColor(sbc),
+    panelBackgroundColor(pbc),
+    panelColor(pc),
+    panelBorderColor(pbdc),
+    screenDividerColor(sdc) {}
+};
+
+inline constexpr ColorPalette SPACEY_PALETTE = {
+    {10, 14, 24, 255}, // deep space navy
+    {22, 31, 45, 255}, // dark blue gray
+    {31, 45, 65, 255}, //slightly lighter panel color
+    {31, 45, 65, 255},
+    {68, 199, 255, 255} // clean cyan accent
+};
+
+inline constexpr ColorPalette WARMER_SPACE_PALETTE = {
+    {13, 17, 23, 255}, // charcoal space black
+    {28, 36, 48, 255}, // warm slate background
+    {42, 52, 66, 255}, // soft steel blue panel
+    {42, 52, 66, 255}, 
+    {255, 183, 77, 255} // amber mission accent
+};
+
+inline constexpr ColorPalette CYBER_HUD = {
+    {8, 12, 20, 255}, // cyber midnight
+    {13, 24, 36, 255}, // deep teal navy
+    {18, 38, 52, 255}, // muted cyber teal
+    {18, 38, 52, 255}, 
+    {0, 255, 200, 255} // neon cyan mint
+};
+
+inline constexpr ColorPalette MISSION_CONTROL_PALETTE = {
+
+    {12, 14, 22, 255},    // near-black navy panel background
+    {12, 14, 22, 255},
+    {25, 30, 45, 255},    // dark aerospace blue panel
+    {90, 105, 130, 255},  // soft steel outline
+    {80, 90, 110, 255} // muted slate divider
+};
--- hud.cpp
+++ hud.cpp
@@ -3,19 +3,19 @@
 #define RLIGHTS_IMPLEMENTATION
 #include "rlights.h"
 #include "tools.hpp"
+#include "colors.hpp"
 
 constexpr int SAMPLE_RATE = 30;
 constexpr int SCREEN_WIDTH = 1200;
-constexpr int SCREEN_HEIGHT = 800;
+constexpr int SCREEN_HEIGHT = 700;
 constexpr int SCENE_WIDTH = SCREEN_WIDTH / 3;
+constexpr int SCENE_RENDER_SCALE = 2;
 constexpr int PANEL_WIDTH = SCREEN_WIDTH - SCENE_WIDTH;
-constexpr int TOP_PANEL_HEIGHT = (SCREEN_HEIGHT / 3) - 20;
-constexpr int BOTTOM_PANEL_HEIGHT = SCREEN_HEIGHT - (TOP_PANEL_HEIGHT + 20) - 20;
+constexpr int TOP_PANEL_HEIGHT = (SCREEN_HEIGHT - 40) / 3;
+constexpr int MIDDLE_PANEL_HEIGHT = TOP_PANEL_HEIGHT;
+constexpr int BOTTOM_PANEL_HEIGHT = TOP_PANEL_HEIGHT;
 
-Color renderingBackground = {20, 20, 30, 255};
-Color panelBackgroundColor = {80, 90, 110, 255};
-Color panelColor = {110, 55, 61, 255};
-Color screenDivideColor = {106, 247, 245, 255};
+const auto *colors = &MISSION_CONTROL_PALETTE;
 float boxRoundness = 0.08;
 
 int main() {
@@ -27,7 +27,8 @@ int main() {
     Rectangle sceneRect = {0, 0, (float)SCENE_WIDTH, (float)SCREEN_HEIGHT};
     Rectangle panelRect = {(float)SCENE_WIDTH, 0, (float)PANEL_WIDTH, (float)SCREEN_HEIGHT};
     Rectangle topPanel = {(float)SCENE_WIDTH + 10, 10, (float)PANEL_WIDTH - 20, (float)TOP_PANEL_HEIGHT};
-    Rectangle bottomPanel = {(float)SCENE_WIDTH + 10, (float)TOP_PANEL_HEIGHT + 20, (float)PANEL_WIDTH, (float)BOTTOM_PANEL_HEIGHT};
+    Rectangle middlePanel = {(float)SCENE_WIDTH + 10, (float)TOP_PANEL_HEIGHT + 20, (float)PANEL_WIDTH - 20, (float)MIDDLE_PANEL_HEIGHT};
+    Rectangle bottomPanel = {(float)SCENE_WIDTH + 10, SCREEN_HEIGHT - (BOTTOM_PANEL_HEIGHT + 10), (float)PANEL_WIDTH - 20, (float)BOTTOM_PANEL_HEIGHT};
 
     Model rocket = LoadModel("resources/models/rocket.glb");
 
@@ -57,7 +58,9 @@ int main() {
     camera.fovy       = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
-    RenderTexture2D sceneTarget = LoadRenderTexture(SCENE_WIDTH, SCREEN_HEIGHT);
+    RenderTexture2D sceneTarget = LoadRenderTexture(SCENE_WIDTH * SCENE_RENDER_SCALE, 
+        SCREEN_HEIGHT * SCENE_RENDER_SCALE);
+    SetTextureFilter(sceneTarget.texture, TEXTURE_FILTER_BILINEAR);
 
     DataRows running_data = get_data("data/log_0007.csv");
 
@@ -86,28 +89,57 @@ int main() {
 
         // --- DRAW ---
         BeginDrawing();
-        ClearBackground(renderingBackground); // dark background makes lighting pop
+        ClearBackground(colors->sceneBackgroundColor); // dark background makes lighting pop
 
         BeginTextureMode(sceneTarget);
-        ClearBackground(renderingBackground);
+        ClearBackground(colors->sceneBackgroundColor);
 
         BeginMode3D(camera);
         DrawModel(rocket, {0, 0, 0}, 0.3f, WHITE);
-        DrawGrid(10, 1.0f);
         EndMode3D();
 
         EndTextureMode();
 
-        DrawTextureRec(
+        DrawTexturePro(
             sceneTarget.texture,
             {0, 0, (float)sceneTarget.texture.width, -(float)sceneTarget.texture.height},
+            sceneRect,
             {0, 0},
+            0.0f,
             WHITE
         );
 
-        DrawRectangleRec(panelRect, panelBackgroundColor);
-        DrawLine(SCENE_WIDTH, 0, SCENE_WIDTH, SCREEN_HEIGHT, screenDivideColor);
-        DrawRectangleRounded(topPanel, boxRoundness, 8, panelColor);
+        // DrawRectangleRec(panelRect, panelBackgroundColor);
+        // DrawLine(SCENE_WIDTH, 0, SCENE_WIDTH, SCREEN_HEIGHT, screenDivideColor);
+        // DrawRectangleRounded(topPanel, boxRoundness, 8, panelColor);
+        // DrawRectangleRounded(bottomPanel, boxRoundness, 8, panelColor);
+        // DrawRectangleRounded(middlePanel, boxRoundness, 8, panelColor);
+
+        DrawRectangleRec(panelRect, colors->panelBackgroundColor);
+        DrawLine(SCENE_WIDTH, 0, SCENE_WIDTH, SCREEN_HEIGHT, colors->screenDividerColor);
+
+        DrawRectangleRounded(topPanel, 0.08f, 8, colors->panelColor);
+        DrawRectangleRoundedLines(topPanel, 0.08f, 8, colors->panelBorderColor);
+        DrawText("ATTITUDE", SCENE_WIDTH + 25, 25, 20, WHITE);
+        DrawText(TextFormat("GX: %.3f", data.gx), SCENE_WIDTH + 25, 65, 18, GREEN);
+        DrawText(TextFormat("GY: %.3f", data.gy), SCENE_WIDTH + 25, 95, 18, GREEN);
+        DrawText(TextFormat("GZ: %.3f", data.gz), SCENE_WIDTH + 25, 125, 18, GREEN);
+
+        DrawRectangleRounded(middlePanel, 0.08f, 8, colors->panelColor);
+        DrawRectangleRoundedLines(middlePanel, 0.08f, 8, colors->panelBorderColor);
+        DrawText("ALTITUDE / BARO", SCENE_WIDTH + 25, 220, 20, WHITE);
+        DrawText(TextFormat("Alt: %.2f m", data.altM), SCENE_WIDTH + 25, 260, 18, LIGHTGRAY);
+        DrawText(TextFormat("Press: %.2f Pa", data.pressPa), SCENE_WIDTH + 25, 290, 18, LIGHTGRAY);
+        DrawText(TextFormat("Temp: %.2f C", data.baroTempC), SCENE_WIDTH + 25, 320, 18, LIGHTGRAY);
+
+        DrawRectangleRounded(bottomPanel, 0.08f, 8, colors->panelColor);
+        DrawRectangleRoundedLines(bottomPanel, 0.08f, 8, colors->panelBorderColor);
+        DrawText("FLIGHT STATE", SCENE_WIDTH + 25, 415, 20, WHITE);
+        DrawText(TextFormat("Time: %.2f s", data.t_us / 1000000.0f), SCENE_WIDTH + 25, 455, 18, LIGHTGRAY);
+        DrawText("Stage: DATA PLAYBACK", SCENE_WIDTH + 25, 485, 18, LIGHTGRAY);
+        DrawText(TextFormat("Sample dt: %lld us", dt), SCENE_WIDTH + 25, 515, 18, LIGHTGRAY);
+
+
         EndDrawing();
     }
 }
