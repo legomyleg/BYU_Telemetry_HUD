1267d8bb1983b8467932f08391075f6a696e5f94
legomyleg <pearsonroymorris@gmail.com>
2026-05-14 22:01:47 -0600

    making a test csv telemetry source

--- src/telemetry/CsvTelemSource.cpp
+++ src/telemetry/CsvTelemSource.cpp
@@ -1,5 +1,17 @@
 #include "CsvTelemSource.hpp"
+#include <iostream>
+#include <string>
+#include <chrono>
+using std::getline, std::string;
 
-CsvTelemSource::CsvTelemSource(int sampleRate, string filePath) {
-    
-} 
+CsvTelemSource::CsvTelemSource(int sampleRate, string filePath)
+    : _sampleRate(sampleRate), _file(filePath), last_read_time(std::chrono::steady_clock::now()) 
+    {
+        if (!_file.is_open()) {
+            std::cerr << "Error: could not open file at \"" + filePath + "\"" << std::endl;
+        }
+    }
+
+string CsvTelemSource::read_available() {
+    return "";
+}
--- src/telemetry/CsvTelemSource.hpp
+++ src/telemetry/CsvTelemSource.hpp
@@ -1,12 +1,18 @@
 #pragma once
 #include "telemetry/telem_source.hpp"
+#include <chrono>
+#include <fstream>
 #include <string>
-using std::string;
+using std::string, std::ifstream;
 
 class CsvTelemSource : public TelemetrySource {
 private:
     int _sampleRate;
-    string _filePath;
+    ifstream _file;
+
+    std::chrono::steady_clock::time_point last_read_time;
+    std::chrono::microseconds accumulated_time;
+
 public:
     CsvTelemSource(int sampleRate, string filePath);
     string read_available() override;
