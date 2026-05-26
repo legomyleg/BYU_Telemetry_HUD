19699778511488f5f7d71512490c2274304d8758
legomyleg <pearsonroymorris@gmail.com>
2026-05-26 13:45:53 -0600

    Changed ring buffer structure

--- include/telemetry/rolling_sample_window.hpp
+++ include/telemetry/rolling_sample_window.hpp
@@ -1,204 +1,121 @@
 #pragma once
+#include <cassert>
 #include <telemetry/telemetry_config.hpp>
 #include <telemetry/sensor_data.hpp>
 #include <raylib.h>
-#include <vector>
+#include <array>
 #include <chrono>
 #include <logging/logger.hpp>
 #include <cstdint>
+#include <iostream>
 
-using std::vector;
+using std::array;
+using std::cerr;
 using std::chrono::microseconds;
 
 using std::uint64_t;
 using microsec = uint64_t;
 
+
 class SampleRingBuffer {
 private:
+    static constexpr size_t BUFFER_CAPACITY = 200;
+    array<SensorData, BUFFER_CAPACITY> _buffer;
 
-    vector<SensorData> _buffer;
+    size_t head = 0;
+    size_t tail = 0;
+    size_t sample_count = 0;
+
     microsec _bufdur;
-    microsec _current_total_dur = 0;
 
-    size_t _next = 0;
-
-    size_t back_one() const {
-        if (_buffer.size() == 0) return 0;
-
-        return (_next + _buffer.size() - 1) % _buffer.size();
+    const SensorData& front() const {
+        assert(!empty());
+        return _buffer[(BUFFER_CAPACITY + head - 1) % BUFFER_CAPACITY];
     }
 
-    size_t up_one() const {
-        if (_buffer.size() == 0) return 0;
-
-        return (_next + 1) % _buffer.size();
+    const SensorData& back() const {
+        assert(!empty());
+        return _buffer[tail];
     }
 
 public:
 
-    SampleRingBuffer(const microsec& buffer_duration_us) : _bufdur(buffer_duration_us) {
-        if (buffer_duration_us < 500000) {
-
-            LOG_WARN(std::string_view("Ring buffer created with duration of less than half a second.") + 
-                    std::string_view(" Creating buffer of one second."));
-
-            _bufdur = ONE_SECOND;
-        } else if (buffer_duration_us > 3000000) {
-
-            LOG_WARN("MAX Buffer size is 3 seconds. Creating buffer of 3 seconds.");
-
-            _bufdur = 3000000;
-
+    explicit SampleRingBuffer(microsec buffer_duration_us) : _bufdur(buffer_duration_us) {
+        if (buffer_duration_us > 3'000'000) {
+            LOG_WARN(std::format("Ring buffer duration too high ({} us). Setting buffer to length 3sec.", buffer_duration_us));
+            _bufdur = 3'000'000;
         }
+    }
 
-        _buffer.resize(180);
+    size_t size() const {
+        return sample_count;
     }
 
     bool empty() const {
-        return _buffer.empty();
+        return sample_count == 0;
     }
 
-    bool full() const {
-        if (_current_total_dur >= _bufdur) return true;
-        return (_bufdur - _current_total_dur) < 50;
+    bool duration_full() const {
+        if (sample_count < 2) return false;
+        auto current_dur = front().t_us - back().t_us;
+        return _bufdur - current_dur < 50;
     }
 
-    void add_sample(const SensorData data) {
-        if (empty()) {
-            _buffer.emplace_back(data);
-            return;
+    void add_sample(const SensorData& data) {
+
+        assert(sample_count == 0 || data.t_us > front().t_us);
+
+        if (sample_count == BUFFER_CAPACITY) {
+            // Add error handling here
+            
+            tail = (tail + 1) % BUFFER_CAPACITY;
+            sample_count--;
         }
 
-        auto& prev = _buffer[back_one()];
-        microsec time_diff = data.t_us - prev.t_us;
+        _buffer[head] = data;
+        head = (head + 1) % BUFFER_CAPACITY;
+        sample_count++;
 
-        if (_current_total_dur + time_diff > _bufdur) {
-
-            auto& forward = _buffer[up_one()];
-            microsec diff_rm = forward.t_us - _buffer[_next].t_us;
-            _current_total_dur -= diff_rm;
-
-            _buffer[_next] = data;
-
-            _next = up_one();
-
-        } else if (_next == 0) {
-            _buffer.emplace_back(data);
-        } else {
-            _buffer.insert(_buffer.begin() + _next, data);
-            _next = up_one();
+        while (sample_count > 0 && data.t_us - _buffer[tail].t_us > _bufdur) {
+            tail = (tail + 1) % BUFFER_CAPACITY;
+            sample_count--;
         }
-
-        _current_total_dur += time_diff;
     }
 
     Vector3 avg_accel(microsec duration) const {
-        if (empty()) return {0,0,0};
 
-        if (duration > _bufdur) {
-            return avg_accel(_bufdur);
-        }
+        if (sample_count == 0) return {0,0,0};
+
+        size_t i = (head + BUFFER_CAPACITY - 1) % BUFFER_CAPACITY;
+        auto newest_time = _buffer[i].t_us;
 
         float sum_x = 0;
         float sum_y = 0;
         float sum_z = 0;
-        int samples_read = 0;
-        microsec time_count = 0;
+        size_t total = 0;
 
-        const auto& prev = _buffer[back_one()];
-        microsec start_time = prev.t_us;
-        size_t i = back_one();
+        for (size_t n=0; n < sample_count; n++) {
+            auto &data = _buffer[i];
 
-        while (time_count < duration) {
-            auto data = _buffer[i];
+            if (newest_time - data.t_us > duration) break;
+
             sum_x += data.ax;
             sum_y += data.ay;
             sum_z += data.az;
 
-            samples_read++;
-            time_count = start_time - data.t_us;
+            total++;
 
-            i = (_buffer.size() + (i - 1)) % _buffer.size();
+            i = (BUFFER_CAPACITY + i - 1) % BUFFER_CAPACITY;
         }
-        
-        return {sum_x / samples_read, sum_y / samples_read, sum_z / samples_read};
+
+        if (total == 0) return {0,0,0};
+
+        return {sum_x / total, sum_y / total, sum_z / total};
     }
 
     Vector3 avg_accel_all() const {
         return avg_accel(_bufdur);
     }
 
-    Vector3 avg_gyro(microsec duration) const {
-        if (empty()) return {0,0,0};
 
-        if (duration > _bufdur) {
-            return avg_gyro(_bufdur);
-        }
-
-        float sum_x = 0;
-        float sum_y = 0;
-        float sum_z = 0;
-        int samples_read = 0;
-        microsec time_count = 0;
-
-        const auto& prev = _buffer[back_one()];
-        microsec start_time = prev.t_us;
-        size_t i = back_one();
-
-        while (time_count < duration) {
-            auto data = _buffer[i];
-            sum_x += data.gx;
-            sum_y += data.gy;
-            sum_z += data.gz;
-
-            samples_read++;
-            time_count = start_time - data.t_us;
-
-            i = (_buffer.size() + (i - 1)) % _buffer.size();
-        }
-        
-        return {sum_x / samples_read, sum_y / samples_read, sum_z / samples_read};
-    }
-
-    Vector3 avg_gyro_all() const {
-        return avg_gyro(_bufdur);
-    }
-
-    float avg_alt_m(microsec duration) const {
-        if (empty()) return 0;
-
-        if (duration > _bufdur) {
-            return avg_alt_m(_bufdur);
-        }
-
-        float sum = 0;
-        int samples_read = 0;
-        microsec time_count = 0;
-
-        const auto& prev = _buffer[back_one()];
-        microsec start_time = prev.t_us;
-        size_t i = back_one();
-
-        while (time_count < duration) {
-            auto data = _buffer[i];
-
-            sum += data.altM;
-
-            samples_read++;
-            time_count = start_time - data.t_us;
-
-            i = (_buffer.size() + (i - 1)) % _buffer.size();
-        }
-        
-        return sum / samples_read;
-    }
-
-    float avg_alt_m_all() const {
-        return avg_alt_m(_bufdur);
-    }
-
-    int percent_full() {
-        float p = (float)_current_total_dur / (float)_bufdur;
-        return static_cast<int>(p * 100.0f);
-    }
 };
