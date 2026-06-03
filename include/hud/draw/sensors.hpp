b8ea71c3e107cdd73e88e9d0a59a8454f88cdc66
legomyleg <pearsonroymorris@gmail.com>
2026-06-02 21:50:38 -0600

    reworked display
    Work on fixing telem.cpp to make it work with new WrittenText struct
    
    Reworked display

--- include/hud/draw/sensors.hpp
+++ include/hud/draw/sensors.hpp
@@ -1,3 +1,4 @@
+#include "hud/config.hpp"
 #include <raylib.h>
 #include <hud/hud_app.hpp>
 #include <hud/colors.hpp>
@@ -18,25 +19,37 @@ inline void DrawSensorsBox(Font font, const HudBox &box, const ColorPalette &col
     Rectangle attitude = {box.x + (margin*2) + textBoxWidth, textBoxY, textBoxWidth, textBoxHeight};
     Rectangle altitude = {box.x + margin*3 + textBoxWidth*2, textBoxY, textBoxWidth, textBoxHeight};
 
-    string velXyz = "X: " + roundedStr(state.velocity.x) +
-            " Y: " + roundedStr(state.velocity.y) +
-            " Z: " + roundedStr(state.velocity.z);
-    string totalVel = "Total Velocity: " + roundedStr(state.total_velocity) + "m/s";
-    string vertVel = "Vertical Velocity: " + roundedStr(state.vertical_velocity_mps) + "m/s";
-    vector<string> vDataVals = {velXyz, totalVel, vertVel};
-    WrittenText velText = {"VELOCITY", vDataVals};
+    Fields::field totalVel;
+    totalVel.title = "Total Velocity";
+    totalVel.val = roundedStr(state.total_velocity);
 
-    string attRoll = "Roll: " + roundedStr(state.attitude.roll);
-    string attPitch = "Pitch: " + roundedStr(state.attitude.pitch);
-    string attYaw = "Yaw: " + roundedStr(state.attitude.yaw);
-    vector<string> attDataVals = {attRoll, attPitch, attYaw};
-    WrittenText attText = {"ATTITUDE", attDataVals};
+    Fields::field vertVel;
+    vertVel.title = "Vertical Velocity";
+    vertVel.val = roundedStr(state.vertical_velocity_mps, 0);
 
-    string altASL = "Altitude ASL: " + roundedStr(state.altitude);
-    string altAGL = "Altitude AGL: " + roundedStr(state.altAGL());
-    WrittenText altText = {"ALTITUDE", {altASL, altAGL}};
+    Fields velocities = {
+        {totalVel, vertVel}
+    };
 
-    DrawFieldsInBox(font, velocity, velText, colors, 10.0f);
-    DrawFieldsInBox(font, attitude, attText, colors, 10.0f);
-    DrawFieldsInBox(font, altitude, altText, colors, 10.0f);
+            // string velXyz = "X: " + roundedStr(state.velocity.x) +
+            //         " Y: " + roundedStr(state.velocity.y) +
+            //         " Z: " + roundedStr(state.velocity.z);
+            // string totalVel = "Total Velocity: " + roundedStr(state.total_velocity) + "m/s";
+            // string vertVel = "Vertical Velocity: " + roundedStr(state.vertical_velocity_mps) + "m/s";
+            // vector<string> vDataVals = {velXyz, totalVel, vertVel};
+            // WrittenText velText = {"VELOCITY", vDataVals};
+
+    // string attRoll = "Roll: " + roundedStr(state.attitude.roll);
+    // string attPitch = "Pitch: " + roundedStr(state.attitude.pitch);
+    // string attYaw = "Yaw: " + roundedStr(state.attitude.yaw);
+    // vector<string> attDataVals = {attRoll, attPitch, attYaw};
+    // WrittenText attText = {"ATTITUDE", attDataVals};
+    //
+    // string altASL = "Altitude ASL: " + roundedStr(state.altitude);
+    // string altAGL = "Altitude AGL: " + roundedStr(state.altAGL());
+    // WrittenText altText = {"ALTITUDE", {altASL, altAGL}};
+
+    DrawFieldsInBox(font, BOX_HEADER_SIZE, velocity, velocities, colors);
+    // DrawFieldsInBox(font, BOX_HEADER_SIZE, attitude, attText, colors);
+    // DrawFieldsInBox(font, BOX_HEADER_SIZE, altitude, altText, colors);
 }
--- include/hud/draw/utils.hpp
+++ include/hud/draw/utils.hpp
@@ -10,13 +10,18 @@ using std::format, std::string;
 
 inline constexpr float OPTICAL_OFFSET = 2.0f;
 
-struct WrittenText {
-    string title;
-    vector<string> lines;
+struct Fields {
+    struct field {
+        string title;
+        string val;
+    };
+
+    vector<field> fields;
 };
 
-inline string roundedStr(float val) {
-    return format("{:.2f}", val);
+inline string roundedStr(float val, int precision=2) {
+    if (precision == 0) return format("{}", static_cast<int>(val));
+    return format("{:.{}f}", val, precision);
 }
 
 inline void DrawTextCenteredEx(Font font, const char *text, Rectangle rect, float fontSize, const Color color) {
@@ -62,29 +67,43 @@ inline void DrawTextCenteredAtXVertical(Font font, const char* text, Rectangle r
     DrawTextPro(font, text, position, Vector2{0,0}, -90.0f, fontSize, TEXT_SPACING, color);
 }
 
-inline void DrawFieldsInBox(Font font, Rectangle& box, WrittenText &text, const ColorPalette &colors, float gapSize) {
+inline void DrawFieldsInBox(Font font, float fontSize, Rectangle& box, Fields &text, const ColorPalette &colors) {
     DrawRectangleRoundedLines(box, BOX_ROUNDNESS, 8, colors.screenDividerColor);
-    DrawTextCenteredToTop(font, text.title.c_str(), box, SUBHEADER_TEXT_SIZE, colors.headerTextColor, FB_TITLE_MARGIN_TOP);
 
+    const int num_gaps = text.fields.size() + 1;
+    const float total_gap_len = box.height - text.fields.size();
+    const float gap_size = total_gap_len / (float)num_gaps;
 
-    float centeredX = (box.x + box.width) / 2.0f;
-    Vector2 nextPos = {centeredX, box.y + SUBHEADER_TEXT_SIZE + (2.0f*FB_TITLE_MARGIN_BOT)};
-    for (const auto & line : text.lines) {
-        DrawTextCenteredAtY(font, line.c_str(), box, READOUT_TEXT_SIZE, colors.textColor, nextPos.y);
-        nextPos.y += READOUT_TEXT_SIZE + gapSize;
+    float margin_to_side = 10.0f;
+    
+    float x_pos_title = box.x + margin_to_side;
+    auto calc_x_pos_side = [=](string text) {
+        auto size = MeasureTextEx(font, text.c_str(), fontSize, TEXT_SPACING);
+        return (box.x + box.width - size.x - margin_to_side);
+    };
+    float y_pos = box.y + gap_size;
+
+    for (const auto &line : text.fields) {
+        Vector2 pos_title = {x_pos_title, y_pos};
+        DrawTextEx(font, line.title.c_str(), pos_title, fontSize, TEXT_SPACING, colors.headerTextColor);
+
+        Vector2 pos_val = {calc_x_pos_side(line.val), y_pos};
+        DrawTextEx(font, line.val.c_str(), pos_val, fontSize, TEXT_SPACING, colors.textColor);
+
+        y_pos += gap_size;
     }
 }
 
-
-inline void DrawFieldsInBox(Font font, Rectangle& box, WrittenText &text, const ColorPalette &colors, float sh_size, float ro_size, float gapSize) {
+inline void DrawFieldsWithSubheaders(Font font, Rectangle& box, Fields::field text, const ColorPalette &colors, float sh_size, float ro_size, float gapSize) {
     DrawRectangleRoundedLines(box, BOX_ROUNDNESS, 8, colors.screenDividerColor);
     DrawTextCenteredToTop(font, text.title.c_str(), box, sh_size, colors.headerTextColor, FB_TITLE_MARGIN_TOP);
 
-
     float centeredX = (box.x + box.width) / 2.0f;
     Vector2 nextPos = {centeredX, box.y + sh_size + (2.0f*FB_TITLE_MARGIN_BOT)};
-    for (const auto & line : text.lines) {
-        DrawTextCenteredAtY(font, line.c_str(), box, ro_size, colors.textColor, nextPos.y);
-        nextPos.y += ro_size + gapSize;
-    }
+    DrawTextCenteredAtY(font, text.val.c_str(), box, ro_size, colors.textColor, nextPos.y);
+
+    // for (const auto & line : text.fields) {
+    //     DrawTextCenteredAtY(font, line.c_str(), box, ro_size, colors.textColor, nextPos.y);
+    //     nextPos.y += ro_size + gapSize;
+    // }
 }
